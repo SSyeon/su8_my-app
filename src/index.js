320627fffb71b4ad2913b9611b187c009bfee0b8
ssyeon <tjdusdlfkrhd@naver.com>
2019-11-03 11:20:57 +0900

    Square prop -> Board state -> Square prop

--- src/index.js
+++ src/index.js
@@ -3,18 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+  constructor(props) {
+    super(props); //반드시 있어야함
+    this.state = {
+      value: null,
+    };
   }
+
+  render() {
+    return (
+      <button   className="square"
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+ }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props); //반드시 있어야함
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+
+    }
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (<Square 
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}/>);
+    }
+    handleClick(i) {
+      const squares = this.state.squares.slice(); //slice는 잘라서 복사한다는 살짝 그런의미
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
   
     render() {
